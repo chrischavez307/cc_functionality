09a36e6b2aceaf0b33e0579dadae24efc5211f9f
chrischavez307 <info@thechrischavez.com>
2015-06-22 12:32:07 -0600

    Create cc_functionality.php

---
+++ cc_functionality.php
@@ -0,0 +1,14 @@
+<?php
+
+	/*
+	Plugin Name: CC Functionality 
+	Plugin URI: http://www.thechrischavez.com/
+	Description: Functionality plugin. No settings to configure.
+	Author: Chris Chavez
+	*/
+	
+	include "inc/title-tags.php";
+	include "inc/links.php";
+	include "inc/dashboard-widget.php";
+
+?>
